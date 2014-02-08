6ff59860cc26c2950fa692f4dff038dc76ead311
markgreenall <info@markgreenall.co.uk>
2014-02-08 01:04:19 +0000

    added error handling

--- init.py
+++ init.py
@@ -4,11 +4,20 @@ import dropbox
 class BoxTaper(object):
 
     def __init__(self, key, secret):
-        self.app_key      = key
-        self.app_secret   = secret
-        self.access_token = ''
-        self.access_token_file = 'OAuth.token'
-        self.client       = ''
+
+        try:
+            if key is '' or secret is "":
+                raise Exception
+
+            self.app_key      = key
+            self.app_secret   = secret
+            self.access_token = ''
+            self.access_token_file = 'OAuth.token'
+            self.client       = ''
+
+        except Exception:
+                print 'FAIL: You must set both the app_key and the app_secret'
+                exit()
 
     """ Authenticate the API """
     def authenticate(self):
@@ -22,30 +31,44 @@ class BoxTaper(object):
 
             else:
                 print "FAIL: File is not formatted correctly."
+                exit()
 
         except IOError:
 
-            flow = dropbox.client.DropboxOAuth2FlowNoRedirect(self.app_key, self.app_secret)
-            authorize_url = flow.start()
-
-            print '1. Go to: ' + authorize_url
-            print '2. Click "Allow" (you might have to log in first)'
-            print '3. Copy the authorization code.'
-
             """ Get the auth key from the user """
-            code = raw_input("Enter the authorization code here: ").strip()
+            try:
+                flow = dropbox.client.DropboxOAuth2FlowNoRedirect(self.app_key, self.app_secret)
+                authorize_url = flow.start()
 
-            """ Finish flow """
-            access_token, user_id = flow.finish(code)
+                print '1. Go to: ' + authorize_url
+                print '2. Click "Allow" (you might have to log in first)'
+                print '3. Copy the authorization code.'
 
-            """ Define into globals """
-            self.access_token = access_token
-            self.user_id      = user_id
+                code = raw_input("Enter the authorization code here: ").strip()
 
-            """ Write the token into the token file """
-            with open(self.access_token_file, 'w') as f:
-                f.write('key->' + access_token)
-                print "PASS: Written Key to Token File"
+                if code is "":
+                    raise Exception
+
+                """ Finish flow """
+                access_token, user_id = flow.finish(code)
+
+                """ Define into globals """
+                self.access_token = access_token
+                self.user_id      = user_id
+
+                """ Write the token into the token file """
+                try:
+                    with open(self.access_token_file, 'w') as f:
+                        f.write('key->' + access_token)
+                        print "PASS: Written Key to Token File"
+
+                except IOError:
+                    print 'FAIL: Could not save the OAuth token into the configured file.'
+                    exit()
+
+            except Exception:
+                print 'FAIL: You did not provide an authorisation code.'
+                exit()
 
     """ Instantiate the client """
     def instantiate(self):
@@ -63,8 +86,16 @@ class BoxTaper(object):
 
     """ Let's put a test file in the Dropbox """
     def put_file(self, contents, file_name):
-        response = self.client.put_file(file_name, contents)
-        print "RESPONSE:\n", response, "\n"
+        try:
+            if contents is "" or file_name is "":
+                raise Exception
+
+            response = self.client.put_file(file_name, contents)
+            print "RESPONSE:\n", response, "\n"
+
+        except Exception:
+            print 'FAIL: You must specify some content, and the file name in which to save it.'
+            exit()
 
 
 app_key    = '04qs1wgvgn99a7d'
